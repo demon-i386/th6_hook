ad4046f44fe0888578cc1fabe273660411b5adb3
demon-i386 <75624951+demon-i386@users.noreply.github.com>
2022-04-19 23:00:18 -0300

    Create offsets.h

---
+++ offsets.h
@@ -0,0 +1,10 @@
+#pragma once
+
+DWORD gameBase;
+DWORD powerOffset = gameBase + 0x29D4B0;
+DWORD scoreOffset = gameBase + 0x29BCA0;
+DWORD timeOffset  = gameBase + 0x29BC4C;
+DWORD grazeOffset = gameBase + 0x29BCB4;
+DWORD usedBombsOffset  = gameBase + 0x69BCC4;
+DWORD bossSecondLifeOffsetView = gameBase + 0x29BC44;
+DWORD pointItemsOffset = gameBase + 0x29D4B4;
