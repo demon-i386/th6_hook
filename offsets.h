8790d6af3da30fb78d5bec50398f6f2ae41f4c25
demon-i386 <75624951+demon-i386@users.noreply.github.com>
2022-04-20 16:49:08 -0300

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -8,3 +8,6 @@ DWORD grazeOffset = gameBase + 0x29BCB4;
 DWORD usedBombsOffset  = gameBase + 0x69BCC4;
 DWORD bossSecondLifeOffsetView = gameBase + 0x29BC44;
 DWORD pointItemsOffset = gameBase + 0x29D4B4;
+DWORD bossLifeOffset   = gameBase + 0xB957C; // 100000 = max value
+DWORD bombCountOffset  = gameBase + 0x29D4BA; // 100000 = max value
+DWORD lifeCountOffset  = gameBase + 0x29D4BA; // ... 772 = 4 life; 771 = 3 life; 770 = 2 lifes; 769 = 1 life
