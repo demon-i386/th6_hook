e98d06743ffc56363446fcc7f99d08475a09b5b9
demon-i386 <75624951+demon-i386@users.noreply.github.com>
2022-04-21 01:34:52 -0300

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,14 +1,14 @@
 #pragma once
 
 DWORD gameBase;
-DWORD powerOffset = gameBase + 0x29D4B0;
+DWORD powerOffset = gameBase + 0x29D4B0;      // 1 byte
 DWORD scoreOffset = gameBase + 0x29BCA0;
 DWORD timeOffset  = gameBase + 0x29BC4C;
 DWORD grazeOffset = gameBase + 0x29BCB4;
 DWORD usedBombsOffset  = gameBase + 0x69BCC4;
 DWORD bossSecondLifeOffsetView = gameBase + 0x29BC44;
 DWORD pointItemsOffset = gameBase + 0x29D4B4;
-DWORD bossLifeOffset   = gameBase + 0xB957C; // 100000 = max value
-DWORD bombCountOffset  = gameBase + 0x29D4BA; // 100000 = max value
-DWORD lifeCountOffset  = gameBase + 0x29D4BA; // ... 772 = 4 life; 771 = 3 life; 770 = 2 lifes; 769 = 1 life
+DWORD bossLifeOffset   = gameBase + 0xB957C;  // 2 bytes
+DWORD bombCountOffset  = gameBase + 0x69D4BB; // 1 byte 
+DWORD lifeCountOffset  = gameBase + 0x69D4BA; // 1 byte 
 DWORD xPositionOffset  = gamebase + 0x2CAAC8; // maybe
