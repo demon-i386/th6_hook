cf6810268302d560d09884c49d810f2b449934d8
demon-i386 <75624951+demon-i386@users.noreply.github.com>
2022-04-21 02:42:58 -0300

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,14 +1,18 @@
 #pragma once
 
-DWORD gameBase;
-DWORD powerOffset = gameBase + 0x29D4B0;      // 1 byte
-DWORD scoreOffset = gameBase + 0x29BCA0;
-DWORD timeOffset  = gameBase + 0x29BC4C;
-DWORD grazeOffset = gameBase + 0x29BCB4;
-DWORD usedBombsOffset  = gameBase + 0x69BCC4;
-DWORD bossSecondLifeOffsetView = gameBase + 0x29BC44;
-DWORD pointItemsOffset = gameBase + 0x29D4B4;
-DWORD bossLifeOffset   = gameBase + 0xB957C;  // 2 bytes
-DWORD bombCountOffset  = gameBase + 0x69D4BB; // 1 byte 
-DWORD lifeCountOffset  = gameBase + 0x69D4BA; // 1 byte 
-DWORD xPositionOffset  = gamebase + 0x2CAAC8; // maybe
+
+struct OffsetStruct {
+	DWORD gameBase;
+	DWORD powerOffset = 0x29D4B0;      // 1 byte
+	DWORD scoreOffset = 0x29BCA0;
+	DWORD timeOffset = 0x29BC4C;
+	DWORD grazeOffset = 0x29BCB4;
+	DWORD usedBombsOffset = 0x69BCC4;
+	DWORD bossSecondLifeOffsetView = 0x29BC44;
+	DWORD pointItemsOffset = 0x29D4B4;
+	DWORD bossLifeOffset = 0xB957C;  // 2 bytes
+	DWORD bombCountOffset = 0x69D4BB; // 1 byte 
+	DWORD lifeCountOffset = 0x69D4BA; // 1 byte 
+	DWORD xPositionOffset = 0x2CAAC8; // maybe
+};
+OffsetStruct offsets;
